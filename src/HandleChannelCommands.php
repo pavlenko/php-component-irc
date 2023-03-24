3e34c73a63cdeaed9e3f43bf2986723cd066ec28
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-03-24 16:25:51 +0200

    upd

---
+++ src/HandleChannelCommands.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PE\Component\IRC;
+
+trait HandleChannelCommands
+{
+    public function handleMODE(CMD $cmd)
+    {}
+
+    public function handleJOIN(CMD $cmd)
+    {}
+
+    public function handleTOPIC(CMD $cmd)
+    {}
+
+    public function handleINVITE(CMD $cmd)
+    {}
+
+    public function handleKICK(CMD $cmd)
+    {}
+
+    public function handlePART(CMD $cmd)
+    {}
+
+    public function handleNAMES(CMD $cmd)
+    {}
+
+    public function handleLIST(CMD $cmd)
+    {}
+}

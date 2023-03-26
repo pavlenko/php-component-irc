64e5a33a34fcd1a23517073fed71125baa074612
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-03-26 18:53:03 +0300

    upd

--- src/Config.php
+++ src/Config.php
@@ -10,23 +10,53 @@ class Config
     private ?string $password;
     private ?string $modtFile;
 
+    private string $info = '';
+
+    private string $adminLocation1;// Typically: country, state and city; can be admin username
+    private string $adminLocation2;// Typically: university and department; can be admin nickname
+    private string $adminEmail;
+
+    private int $maxChannels;
+    private int $maxInactiveTimeout;
+    private int $maxResponseTimeout;
+
     public function __construct(
         string $name,
         \DateTimeInterface $createdAt,
         string $version,
         string $password = null,
-        string $modtFile = null
-    ) {
+        string $modtFile = null,
+        $adminLocation1 = '',
+        $adminLocation2 = '',
+        $adminEmail = '',
+        int $maxChannels = 0,
+        int $maxInactiveTimeout = 0,
+        int $maxResponseTimeout = 0
+) {
         $this->name = $name;
         $this->createdAt = $createdAt;
         $this->version = $version;
         $this->password = $password;
         $this->modtFile = $modtFile;
+
+        $this->adminLocation1 = $adminLocation1;
+        $this->adminLocation2 = $adminLocation2;
+        $this->adminEmail     = $adminEmail;
+
+
+        $this->maxChannels        = $maxChannels;
+        $this->maxInactiveTimeout = $maxInactiveTimeout;
+        $this->maxResponseTimeout = $maxResponseTimeout;
     }
 
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public function getInfo(): string
+    {
+        return $this->info;
     }
 
     public function getVersion(): string
@@ -50,5 +80,35 @@ class Config
             return file($this->modtFile, FILE_IGNORE_NEW_LINES) ?: null;
         }
         return null;
+    }
+
+    public function getAdminLocation1(): string
+    {
+        return $this->adminLocation1;
+    }
+
+    public function getAdminLocation2(): string
+    {
+        return $this->adminLocation2;
+    }
+
+    public function getAdminEmail(): string
+    {
+        return $this->adminEmail;
+    }
+
+    public function getMaxChannels(): int
+    {
+        return $this->maxChannels;
+    }
+
+    public function getMaxInactiveTimeout(): int
+    {
+        return $this->maxInactiveTimeout;
+    }
+
+    public function getMaxResponseTimeout(): int
+    {
+        return $this->maxResponseTimeout;
     }
 }

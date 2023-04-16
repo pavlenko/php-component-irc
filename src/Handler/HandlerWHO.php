d246805c77261203ff5cf6774b0dcfc0ff903e81
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-04-16 10:38:51 +0300

    upd

--- src/Handler/HandlerWHO.php
+++ src/Handler/HandlerWHO.php
@@ -14,8 +14,8 @@ final class HandlerWHO implements HandlerInterface
     //TODO :<prefix> <rpl> <channel> <user> <host> <server> <nick> <H|G>[*][@|+] :<hop count> <real name>
     public function __invoke(CMD $cmd, SessionInterface $sess, StorageInterface $stor): int
     {
+        $sessions = [];
         if ($cmd->numArgs() === 0) {
-            $who = [];
             foreach ($stor->sessions() as $s) {
                 if ($sess === $s) {
                     continue;
@@ -30,13 +30,32 @@ final class HandlerWHO implements HandlerInterface
                 if ($hasCommonChannel) {
                     continue;
                 }
-                //TODO
+                $sessions[] = $s;
             }
-            //TODO generate reply by channel+user key
+        } else {
+            $chan = $stor->channels()->searchByName($cmd->getArg(0));
+            if (null !== $chan) {
+                foreach ($chan->getSessions($stor) as $user) {
+                    $this->replyWHO($cmd, $sess, $chan, $user);
+                }
+                return $sess->sendRPL(RPL::RPL_END_OF_WHO, $cmd->getArg(0));
+            } else {
+                //TODO list channels & end of who per match sessions
+                foreach ($stor->sessions() as $s) {
+                    $equal = $stor->isEqualToRegex($cmd->getArg(0), $s->getHostname())
+                        || $stor->isEqualToRegex($cmd->getArg(0), $s->getServername())
+                        || $stor->isEqualToRegex($cmd->getArg(0), $s->getRealname())
+                        ||$stor->isEqualToRegex($cmd->getArg(0), $s->getNickname());
+
+                    if ($equal) {
+                        $sessions[] = $s;
+                    }
+                }
+                return 0;
+            }
         }
 
-        //TODO WHO -> list all visible users with no same channels
-        //TODO WHO name -> check channel, else match user
+        $sessions = array_filter($sessions, fn($s) => !$s->hasFlag(SessionInterface::FLAG_INVISIBLE));//
 
         //TODO end of who must follow each list item if arg0 passed, elsewhere without name at end of reply
 return 0;
@@ -83,5 +102,28 @@ return 0;
             }
         }
         return $sess->sendRPL(RPL::RPL_END_OF_WHO);
+    }
+
+    private function replyWHO(CMD $cmd, SessionInterface $sess, ChannelInterface $chan, SessionInterface $user): void
+    {
+        if ($cmd->getArg(1) === 'o' && !$user->hasFlag(SessionInterface::FLAG_IRC_OPERATOR)) {
+            return;
+        }
+
+        $userStatus = '';
+        if ($chan->hasOperator($user)) {
+            $userStatus = '@';
+        } elseif ($chan->hasSpeaker($user)) {
+            $userStatus = '+';
+        }
+
+        $sess->sendRPL(RPL::RPL_WHO_REPLY, [
+            $chan->getName(),
+            $user->getUsername(),
+            $user->getHostname(),
+            $user->getServername(),
+            $user->getNickname(),
+            'H' . $userStatus,
+        ], '0 ' . $user->getRealname());
     }
 }

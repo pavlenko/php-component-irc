f50f2504d430f6a61fd52d2a3077857cfa933392
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-04-19 06:14:50 +0300

    upd

---
+++ src/ClientInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace PE\Component\IRC;
+
+/**
+ * @property $password
+ * @property $nickname
+ * @property $username
+ * @property $realname
+ *
+ * @property $servername
+ *
+ * @property $flags
+ * @property $channels
+ *
+ * @property $registrationTime //TODO rename
+ * @property $lastPingingTime
+ * @property $lastMessageTime
+ *
+ * @property $awayMessage
+ * @property $quitMessage
+ */
+interface ClientInterface
+{
+}

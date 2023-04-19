3a0a5472e1b8198b9b3fa139f2e4314f3d12e10f
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-04-19 06:46:11 +0300

    upd

--- src/ServerInterface.php
+++ src/ServerInterface.php
@@ -11,4 +11,11 @@ namespace PE\Component\IRC;
  */
 interface ServerInterface
 {
+    //PASS <password> <version> <flags> [<options>]
+    //SERVER <servername> <hopcount> <token> <info>
+    //NICK <nickname> <hopcount> <username> <host> <servertoken> <umode> <realname>
+    //SERVICE <servicename> <servertoken> <distribution> <type> <hopcount> <info>
+    //QUIT [:<Quit Message>]
+    //SQUIT <server> <comment>
+    //NJOIN <channel> [ "@@" / "@" ] [ "+" ] <nickname> *( "," [ "@@" / "@" ] [ "+" ] <nickname> )
 }

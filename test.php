70c74043751eb58da44b1ef67dc38a610a9d5fec
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-04-20 20:08:57 +0300

    upd

--- test.php
+++ test.php
@@ -5,25 +5,31 @@ namespace PE\Component\IRC;
 use PE\Component\Stream\Factory;
 use PE\Component\Stream\Select;
 use PE\Component\Stream\Socket;
+use PE\Component\Stream\Stream;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $active = true;
 $stream = (new Factory())->createClient('tls://irc.libera.chat:6697');
 $select = new Select();
-$socket = new Socket($stream, $select);
-$socket->onInput(function ($message) {
-    echo $message . "\n";
+$select->attachStreamWR($stream, function (Stream $stream, Select $select) use (&$active) {
+    $select->detachStreamWR($stream);
+    echo "!: Connected to remote {$stream->getAddress(true)}\n";
+
+    $socket = new Socket($stream, $select);
+    $socket->onInput(function ($message) {
+        echo $message . "\n";
+    });
+    $socket->onError(function ($message) {
+        echo $message . "\n";
+    });
+    $socket->onClose(function ($message) use (&$active) {
+        $active = false;
+        echo $message . "\n";
+    });
+    $socket->write((new CMD(CMD::PASSWORD, ['pass', '0201', 'IRC|']))->toString() . "\n");
+    $socket->write((new CMD(CMD::SERVER, ['srv0', '0', '999'], 'just test'))->toString() . "\n");
 });
-$socket->onError(function ($message) {
-    echo $message . "\n";
-});
-$socket->onClose(function ($message) use (&$active) {
-    $active = false;
-    echo $message . "\n";
-});
-$socket->write((new CMD(CMD::PASSWORD, ['pass', '0201', 'IRC|']))->toString() . "\n");
-$socket->write((new CMD(CMD::SERVER, ['srv0', '0', '999'], 'just test'))->toString() . "\n");
 
 while ($active) {
     $select->dispatch();

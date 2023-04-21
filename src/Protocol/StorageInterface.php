2f2c041d478a9c0e13922c1163129af016129d8f
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-04-21 20:03:44 +0300

    upd

--- src/Protocol/StorageInterface.php
+++ src/Protocol/StorageInterface.php
@@ -4,6 +4,7 @@ namespace PE\Component\IRC\Protocol;
 
 interface StorageInterface
 {
+    //TODO create dto models for each state part, try to not use collections on use only them as typed collection
     //commands - collect call count & payload bytes in tx/rx
     //sessions - all sessions (registered and stale) - add role param
     //- client
@@ -12,4 +13,14 @@ interface StorageInterface
     //- server
     //channels - channels state
     //history - log client activity history, maybe also remote server
+
+    /**
+     * TODO try use doc as below for valid code navigation
+     * @template T
+     *
+     * @param string $request
+     * @param T $target
+     * @return T
+     */
+    public function template(string $request, $target);
 }

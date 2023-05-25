e9e5b07c9a8b807ceb7288d781eab13aecbfc6e8
Pavlenko Eugene <pavlenko.obs@gmail.com>
2023-05-25 17:50:08 +0300

    add connection code

--- src/Protocol/Connection.php
+++ src/Protocol/Connection.php
@@ -5,8 +5,10 @@ namespace PE\Component\IRC\Protocol;
 use PE\Component\IRC\CMD;
 use PE\Component\IRC\Config;
 use PE\Component\IRC\Deferred;
+use PE\Component\IRC\ERR;
 use PE\Component\IRC\Exception\TimeoutException;
 use PE\Component\IRC\MSG;
+use PE\Component\IRC\RPL;
 
 final class Connection
 {
@@ -25,13 +27,69 @@ final class Connection
         int $responseTimeout = Config::DEFAULT_RESPONSE_TIMEOUT,
         int $inactiveTimeout = Config::DEFAULT_INACTIVE_TIMEOUT
     ) {
-        $this->socket          = $socket;
         $this->responseTimeout = $responseTimeout;
         $this->inactiveTimeout = $inactiveTimeout;
 
         $this->onInput = fn() => null;
         $this->onError = fn() => null;
         $this->onClose = fn() => null;
+
+        $this->socket = $socket;
+        $this->socket->setCloseHandler(fn(string $message = null) => $this->close($message));
+        $this->socket->setErrorHandler($this->onError);
+        $this->socket->setInputHandler(function (string $data) {
+            try {
+                $this->onInput($data);
+            } catch (\Throwable $exception) {
+                call_user_func($this->onError, $exception);
+            }
+        });
+    }
+
+    private function onInput(string $input): void
+    {
+        $lines = preg_split('/\n/', $input, 0, PREG_SPLIT_NO_EMPTY);
+        foreach ($lines as $line) {
+            try {
+                $msg = $this->decode(trim($line));
+                call_user_func($this->onInput, $msg);
+            } catch (\Throwable $error) {
+                call_user_func($this->onError, $error, $line);
+            }
+        }
+    }
+
+    private function decode(string $data): MSG
+    {
+        $parts = preg_split('/\s+/', $data, 2, PREG_SPLIT_NO_EMPTY);
+
+        // Resolve prefix
+        $prefix = null;
+        if (!empty($parts) && ':' === $parts[0][0]) {
+            $prefix = substr($parts[0], 1);
+            $data   = $parts[1] ?? '';
+        }
+
+        // Resolve command
+        $parts = preg_split('/\s+/', $data, 2, PREG_SPLIT_NO_EMPTY);
+        $code  = strtoupper(array_shift($parts) ?? '');
+
+        if (empty($code)) {
+            throw new \UnexpectedValueException('Malformed data, no command code exists');
+        }
+
+        // Resolve comment & params
+        $parts   = preg_split('/:/', $parts[0] ?? '', 2, PREG_SPLIT_NO_EMPTY);
+        $args    = preg_split('/\s+/', $parts[0] ?? '', 0, PREG_SPLIT_NO_EMPTY);
+        $comment = !empty($parts[1]) ? trim($parts[1]) : null;
+
+        if (is_numeric($code)) {
+            if ($code < 400) {
+                return new RPL($prefix, $code, $args, $comment);
+            }
+            return new ERR($prefix, $code, $args, $comment);
+        }
+        return new CMD($code, $args, $comment, $prefix);
     }
 
     /**
@@ -65,8 +123,15 @@ final class Connection
         $this->socket->write($msg->toString(), $close);
     }
 
-    public function close(): void
+    public function close(string $message = null): void
     {
+        call_user_func($this->onClose, $message);
+
+        $this->socket->setCloseHandler(fn() => null);
         $this->socket->close();
+
+        $this->onInput = fn() => null;
+        $this->onError = fn() => null;
+        $this->onClose = fn() => null;
     }
 }
